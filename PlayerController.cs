5d09f464f481926d1d4aaba4a31515e7015b9b35
Bruh <177808919+Bruxki@users.noreply.github.com>
2024-10-10 12:14:24 +0600

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     public float turnSpeed;
-    public float horizontalInput
+    public float horizontalInput;
+    public float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         
         transform.Translate(Vector3.back * Time.deltaTime * speed);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
